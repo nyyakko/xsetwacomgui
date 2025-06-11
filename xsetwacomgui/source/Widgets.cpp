0d9cdb4e887c67deaa628ce9fae7231d6b88a373
nyyakko <nyakonyns@gmail.com>
2025-06-11 20:24:37 -0300

    remove unnecessary indirections

--- xsetwacomgui/source/Widgets.cpp
+++ xsetwacomgui/source/Widgets.cpp
@@ -3,25 +3,13 @@
 
 static auto constexpr MAPPER_GRAB_RADIUS = 6;
 
-#define MAPPER_BACKGROUD_COLOR   ImColor(ImGui::GetStyle().Colors[ImGuiCol_FrameBg])
-#define MAPPER_GRID_COLOR        ImColor(ImGui::GetStyle().Colors[ImGuiCol_FrameBgHovered])
-#define MAPPER_GRAB_COLOR        ImColor(ImGui::GetStyle().Colors[ImGuiCol_SliderGrab])
-#define MAPPER_GRAB_ACTIVE_COLOR ImColor(ImGui::GetStyle().Colors[ImGuiCol_SliderGrabActive])
+#define MAPPER_BACKGROUD_COLOR          ImColor(ImGui::GetStyle().Colors[ImGuiCol_FrameBg])
+#define MAPPER_BACKGROUD_CONTRAST_COLOR ImColor(ImGui::GetStyle().Colors[ImGuiCol_MenuBarBg])
+#define MAPPER_GRID_COLOR               ImColor(ImGui::GetStyle().Colors[ImGuiCol_FrameBgHovered])
+#define MAPPER_GRAB_COLOR               ImColor(ImGui::GetStyle().Colors[ImGuiCol_SliderGrab])
+#define MAPPER_GRAB_ACTIVE_COLOR        ImColor(ImGui::GetStyle().Colors[ImGuiCol_SliderGrabActive])
 
-static void area_mapper_render_grid(ImDrawList* drawList, ImRect frame, ImVec2 size)
-{
-    for (size_t i = 0; i <= static_cast<size_t>(size.x); i += static_cast<size_t>(size.x / 4))
-        drawList->AddLine({ frame.Min.x + static_cast<float>(i), frame.Min.y }, { frame.Min.x + static_cast<float>(i), frame.Max.y }, MAPPER_GRID_COLOR);
-    for (size_t i = 0; i <= static_cast<size_t>(size.y); i += static_cast<size_t>(size.y / 4))
-        drawList->AddLine({ frame.Min.x, frame.Min.y + static_cast<float>(i) }, { frame.Max.x, frame.Min.y + static_cast<float>(i) }, MAPPER_GRID_COLOR);
-}
-
-static void area_mapper_render_region(ImGuiStyle& style, ImRect frame, ImVec2 const anchors[4])
-{
-    ImGui::RenderFrame(anchors[0] * (frame.Max - frame.Min) + frame.Min, anchors[3] * (frame.Max - frame.Min) + frame.Min, MAPPER_BACKGROUD_COLOR, true, style.FrameRounding);
-}
-
-static bool area_mapper_render_grabbers(ImDrawList* const drawList, ImRect frame, ImVec2 size, char const* label, ImVec2 anchors[4], bool fullArea = false, bool = false)
+static bool area_mapper_render_grabbers(ImDrawList* const drawList, ImRect frame, ImVec2 size, ImVec2 anchors[4], char const* label, bool fullArea = false, bool = false)
 {
     bool changed = false;
 
@@ -79,7 +67,11 @@ static bool area_mapper_render_grabbers(ImDrawList* const drawList, ImRect frame
 bool area_mapper(char const* label, ImVec2 anchors[4], ImVec2 size, ImRect* outPosition, bool forceFullArea, bool forceAspectRatio)
 {
     auto window = ImGui::GetCurrentWindow();
-    if (window->SkipItems) return false;
+
+    if (window->SkipItems)
+    {
+        return false;
+    }
 
     bool changed = false;
 
@@ -92,18 +84,29 @@ bool area_mapper(char const* label, ImVec2 anchors[4], ImVec2 size, ImRect* outP
     frameExtended.Max += ImVec2(MAPPER_GRAB_RADIUS, MAPPER_GRAB_RADIUS) / 2;
     ImGui::ItemSize(frameExtended);
 
-    if (outPosition) *outPosition = frame;
+    if (outPosition)
+    {
+        *outPosition = frame;
+    }
 
     if (ImGui::ItemAdd(frameExtended, 0))
     {
-        ImGuiStyle& style = ImGui::GetStyle();
         ImDrawList* drawList = ImGui::GetWindowDrawList();
 
-        ImGui::RenderFrame(frame.Min, frame.Max, ImColor(ImGui::GetStyle().Colors[ImGuiCol_MenuBarBg]), true, style.FrameRounding);
+        ImGui::RenderFrame(frame.Min, frame.Max, MAPPER_BACKGROUD_CONTRAST_COLOR);
+        ImGui::RenderFrame(anchors[0] * (frame.Max - frame.Min) + frame.Min, anchors[3] * (frame.Max - frame.Min) + frame.Min, MAPPER_BACKGROUD_COLOR);
 
-        area_mapper_render_region(style, frame, anchors);
-        area_mapper_render_grid(drawList, frame, size);
-        changed = area_mapper_render_grabbers(drawList, frame, size, label, anchors, forceFullArea, forceAspectRatio);
+        for (size_t i = 0; i <= static_cast<size_t>(size.x); i += static_cast<size_t>(size.x / 4))
+        {
+            drawList->AddLine({ frame.Min.x + static_cast<float>(i), frame.Min.y }, { frame.Min.x + static_cast<float>(i), frame.Max.y }, MAPPER_GRID_COLOR);
+        }
+
+        for (size_t i = 0; i <= static_cast<size_t>(size.y); i += static_cast<size_t>(size.y / 4))
+        {
+            drawList->AddLine({ frame.Min.x, frame.Min.y + static_cast<float>(i) }, { frame.Max.x, frame.Min.y + static_cast<float>(i) }, MAPPER_GRID_COLOR);
+        }
+
+        changed = area_mapper_render_grabbers(drawList, frame, size, anchors, label, forceFullArea, forceAspectRatio);
     }
 
     ImGui::EndGroup();
