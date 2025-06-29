a1bf0c8e205e7c94ce44b9bdc5bdccae6bbf0022
nyyakko <nyakonyns@gmail.com>
2025-06-28 22:32:18 -0300

    update cmake scripts

--- cmake/find_xrandr.cmake
+++
@@ -1,9 +0,0 @@
-function(find_xrandr)
-    find_program(XRANDR_EXECUTABLE xrandr)
-
-    if (NOT XRANDR_EXECUTABLE)
-        message(FATAL_ERROR "xrandr could not be found. try installing it before proceeding, also be advised that this program is intended to be run under X11.")
-    else()
-        message(STATUS "found xrandr: ${XRANDR_EXECUTABLE}")
-    endif()
-endfunction()
--- cmake/find_xsetwacom.cmake
+++
@@ -1,9 +0,0 @@
-function(find_xsetwacom)
-    find_program(XSETWACOM_EXECUTABLE xsetwacom)
-
-    if (NOT XSETWACOM_EXECUTABLE)
-        message(FATAL_ERROR "xsetwacom could not be found. try installing the wacom driver before proceeding, also be advised that this program is intended to be run under X11.")
-    else()
-        message(STATUS "found xsetwacom: ${XSETWACOM_EXECUTABLE}")
-    endif()
-endfunction()
