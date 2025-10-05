af85ddaff7f89c73c49fce6e8cce651184d37759
nyyakko <nyakonyns@gmail.com>
2025-10-05 18:47:27 -0300

    add localisation

--- xsetwacomgui/source/ProfileWindow.cpp
+++ xsetwacomgui/source/ProfileWindow.cpp
@@ -12,30 +12,29 @@ using namespace liberror;
 
 using namespace std::literals;
 
-// cppcheck-suppress [constParameterReference]
 Result<void> render_profile_window(Context& context)
 {
-    ImGui::Text("Name");
+    ImGui::Text("%s", TRY(Localisation::get(context.settings.application.language, Localisation::Window_Profile_Tab_Name)));
     static std::array<char, 256> profileName;
     ImGui::SetNextItemWidth(300_scaled + ImGui::GetStyle().WindowPadding.x);
     ImGui::InputText("##ProfileName", profileName.data(), profileName.size());
 
     auto previousCursorPosition = ImGui::GetCursorPos();
     ImGui::SetCursorPosY(ImGui::GetWindowHeight() - (25_scaled + ImGui::GetStyle().WindowPadding.x));
-    if (ImGui::Button("Create", { 150_scaled, 25_scaled }))
+    if (ImGui::Button(TRY(Localisation::get(context.settings.application.language, Localisation::Create)), { 150_scaled, 25_scaled }))
     {
         if (profileName.data() == ""sv)
         {
             ImGui::PushToast(
                 TRY(Localisation::get(context.settings.application.language, Localisation::Toast_Error)),
-                "Profile name cannot be empty"
+                TRY(Localisation::get(context.settings.application.language, Localisation::Toast_Profile_Name_Empty))
             );
         }
         else
         {
             ImGui::PushToast(
                 TRY(Localisation::get(context.settings.application.language, Localisation::Toast_Success)),
-                "Profile created successfully"
+                TRY(Localisation::get(context.settings.application.language, Localisation::Toast_Profile_Created))
             );
             context.settings.tablet.profiles.emplace(profileName.data(), TRY(TabletSettings::Profile::make_default(context.tablet, context.display)));
             save_tablet_settings(context.settings.tablet);
