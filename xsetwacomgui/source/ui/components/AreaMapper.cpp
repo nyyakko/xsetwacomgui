8e280ddcf80d7f448d1a2e95e42fa68e85a8103e
nyyakko <nyakonyns@gmail.com>
2025-10-06 20:33:13 -0300

    inline code

--- xsetwacomgui/source/ui/components/AreaMapper.cpp
+++ xsetwacomgui/source/ui/components/AreaMapper.cpp
@@ -9,63 +9,7 @@ static auto constexpr MAPPER_GRAB_RADIUS = 6;
 #define MAPPER_GRAB_COLOR               ImColor(ImGui::GetStyle().Colors[ImGuiCol_SliderGrab])
 #define MAPPER_GRAB_ACTIVE_COLOR        ImColor(ImGui::GetStyle().Colors[ImGuiCol_SliderGrabActive])
 
-static bool area_mapper_grabbers(ImDrawList* const drawList, ImRect frame, ImVec2 size, ImVec2 anchors[4], char const* label, bool fullArea = false, bool = false)
-{
-    // FIXME: implement force proportion logic
-    bool changed = false;
-
-    for (size_t i = 0; i < 4; i += 1)
-    {
-        ImVec2 anchor = anchors[i] * (frame.Max - frame.Min) + frame.Min;
-        ImGui::SetCursorScreenPos(anchor - ImVec2(MAPPER_GRAB_RADIUS, MAPPER_GRAB_RADIUS));
-        char anchorTooltip[256] {};
-        snprintf(anchorTooltip, sizeof(anchorTooltip), "%zu##%s", i, label);
-        ImGui::InvisibleButton(anchorTooltip, { 2 * MAPPER_GRAB_RADIUS, 2 * MAPPER_GRAB_RADIUS });
-
-        bool active = ImGui::IsItemActive() || ImGui::IsItemHovered();
-
-        if (active)
-        {
-            ImGui::SetTooltip("(%4.3f, %4.3f)", static_cast<double>(anchors[i].x), static_cast<double>(anchors[i].y));
-            ImGui::SetMouseCursor(ImGuiMouseCursor_Hand);
-        }
-
-        if (!fullArea && active && ImGui::IsMouseDragging(ImGuiMouseButton_Left))
-        {
-            anchors[i] += ImGui::GetIO().MouseDelta / size;
-            anchors[i] = { ImClamp(anchors[i].x, 0.f, 1.f), ImClamp(anchors[i].y, 0.f, 1.f) };
-
-            if (i == 0)
-            {
-                anchors[1].x = anchors[i].x;
-                anchors[2].y = anchors[i].y;
-            }
-            else if (i == 1)
-            {
-                anchors[0].x = anchors[i].x;
-                anchors[3].y = anchors[i].y;
-            }
-            else if (i == 2)
-            {
-                anchors[3].x = anchors[i].x;
-                anchors[0].y = anchors[i].y;
-            }
-            else if (i == 3)
-            {
-                anchors[2].x = anchors[i].x;
-                anchors[1].y = anchors[i].y;
-            }
-
-            changed = true;
-        }
-
-        drawList->AddCircleFilled(anchor, MAPPER_GRAB_RADIUS, active ? MAPPER_GRAB_ACTIVE_COLOR : MAPPER_GRAB_COLOR);
-    }
-
-    return changed;
-}
-
-bool AreaMapper(char const* label, ImVec2 anchors[4], ImVec2 size, ImRect* outPosition, bool forceFullArea, bool forceAspectRatio)
+bool AreaMapper(char const* label, ImVec2 anchors[4], ImVec2 size, ImRect* outPosition, bool forceFullArea, bool)
 {
     auto window = ImGui::GetCurrentWindow();
 
@@ -107,11 +51,56 @@ bool AreaMapper(char const* label, ImVec2 anchors[4], ImVec2 size, ImRect* outPo
             drawList->AddLine({ frame.Min.x, frame.Min.y + static_cast<float>(i) }, { frame.Max.x, frame.Min.y + static_cast<float>(i) }, MAPPER_GRID_COLOR);
         }
 
-        changed = area_mapper_grabbers(drawList, frame, size, anchors, label, forceFullArea, forceAspectRatio);
+        for (size_t i = 0; i < 4; i += 1)
+        {
+            ImVec2 anchor = anchors[i] * (frame.Max - frame.Min) + frame.Min;
+            ImGui::SetCursorScreenPos(anchor - ImVec2(MAPPER_GRAB_RADIUS, MAPPER_GRAB_RADIUS));
+            char anchorTooltip[256] {};
+            snprintf(anchorTooltip, sizeof(anchorTooltip), "%zu##%s", i, label);
+            ImGui::InvisibleButton(anchorTooltip, { 2 * MAPPER_GRAB_RADIUS, 2 * MAPPER_GRAB_RADIUS });
+
+            bool active = ImGui::IsItemActive() || ImGui::IsItemHovered();
+
+            if (active)
+            {
+                ImGui::SetTooltip("(%4.3f, %4.3f)", static_cast<double>(anchors[i].x), static_cast<double>(anchors[i].y));
+                ImGui::SetMouseCursor(ImGuiMouseCursor_Hand);
+            }
+
+            if (!forceFullArea && active && ImGui::IsMouseDragging(ImGuiMouseButton_Left))
+            {
+                anchors[i] += ImGui::GetIO().MouseDelta / size;
+                anchors[i] = { ImClamp(anchors[i].x, 0.f, 1.f), ImClamp(anchors[i].y, 0.f, 1.f) };
+
+                if (i == 0)
+                {
+                    anchors[1].x = anchors[i].x;
+                    anchors[2].y = anchors[i].y;
+                }
+                else if (i == 1)
+                {
+                    anchors[0].x = anchors[i].x;
+                    anchors[3].y = anchors[i].y;
+                }
+                else if (i == 2)
+                {
+                    anchors[3].x = anchors[i].x;
+                    anchors[0].y = anchors[i].y;
+                }
+                else if (i == 3)
+                {
+                    anchors[2].x = anchors[i].x;
+                    anchors[1].y = anchors[i].y;
+                }
+
+                changed = true;
+            }
+
+            drawList->AddCircleFilled(anchor, MAPPER_GRAB_RADIUS, active ? MAPPER_GRAB_ACTIVE_COLOR : MAPPER_GRAB_COLOR);
+        }
     }
 
     ImGui::EndGroup();
 
     return changed;
 }
-
