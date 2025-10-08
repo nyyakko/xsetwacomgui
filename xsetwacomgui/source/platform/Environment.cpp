4d0b9ee9e15f8ea6d7ccc7afeb170edb99aa3d47
nyyakko <nyakonyns@gmail.com>
2025-10-08 20:37:27 -0300

    fix languages path

--- xsetwacomgui/source/platform/Environment.cpp
+++ xsetwacomgui/source/platform/Environment.cpp
@@ -46,8 +46,8 @@ std::filesystem::path get_application_languages_path()
     return std::filesystem::path(HOME) / "resources" / "languages";
 #else
     auto dataHome = getenv("XDG_DATA_HOME");
-    if (dataHome) return std::filesystem::path(dataHome) / NAME / "resources" / "languages";
-    return get_system_home_path() / ".local" / "share" / NAME / "resources" / "languages";
+    if (dataHome) return std::filesystem::path(dataHome) / NAME / "languages";
+    return get_system_home_path() / ".local" / "share" / NAME / "languages";
 #endif
 
 }
