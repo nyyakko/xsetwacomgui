e9943101183b2417c519cae07b58eaa41e24b81d
nyyakko <nyakonyns@gmail.com>
2025-10-24 21:33:17 -0300

    generate .desktop file through cmake

--- install.py
+++ install.py
@@ -2,29 +2,6 @@ import os
 import sys
 import configparser
 
-def configure_desktop_metadata(prefix):
-    meta = configparser.ConfigParser()
-    meta.optionxform = str
-
-    meta['Desktop Entry'] = {
-        'Version': '1.0',
-        'Type': 'Application',
-        'Name': 'XSetWacomGUI',
-        'Comment': 'A graphical xsetwacom wrapper for ease of use',
-        'Exec': f'{prefix}/bin/xsetwacomgui',
-        'Icon': 'xsetwacomgui.png',
-        'Terminal': 'false',
-        'Categories': 'Wacom;Drawing;Graphics;Utility;'
-    }
-
-    return meta
-
-def install_desktop_metadata(prefix):
-    meta = configure_desktop_metadata(prefix)
-    file = open(f'{prefix}/share/applications/xsetwacomgui.desktop', 'w+')
-    meta.write(file)
-    file.close()
-
 def main(arguments):
     prefix = "~/.local"
 
@@ -32,7 +9,6 @@ def main(arguments):
         prefix = arguments[0]
 
     os.system(f'cmake --install build --prefix {prefix}')
-    install_desktop_metadata(os.path.expanduser(prefix))
 
 if __name__ == "__main__":
     sys.argv.pop(0)
